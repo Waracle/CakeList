c1656b6d54c1ab85142d59a6590aed6dc1e359e0
Harmeet Singh <singh_120@hotmail.co.uk>
2018-04-05 00:46:33 +0100

    feature/Crash-Fix: Fixed crash by using correct cell identifier

--- Cake List/MasterViewController.m
+++ Cake List/MasterViewController.m
@@ -30,7 +30,7 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    CakeCell *cell = (CakeCell*)[tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    CakeCell *cell = (CakeCell*)[tableView dequeueReusableCellWithIdentifier:@"CakeCell"];
     
     NSDictionary *object = self.objects[indexPath.row];
     cell.titleLabel.text = object[@"title"];
